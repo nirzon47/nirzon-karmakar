d94bb0480fef569f983571b50201990f5ba62f30
Nirzon Karmakar <nirzon47@gmail.com>
2024-04-11 19:58:47 +0530

    refactor: component tree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import LandingText from '@/components/landing/LandingText'
 export default function Home() {
 	return (
 		<div className='gradient-background'>
-			<LandingText />
 			<Landing />
 		</div>
 	)
--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,10 +1,12 @@
 import dynamic from 'next/dynamic'
+import LandingText from './LandingText'
 
 const StarrySky = dynamic(() => import('./StarrySky'), { ssr: false })
 
 const Landing = () => {
 	return (
 		<div className='relative'>
+			<LandingText />
 			<StarrySky />
 		</div>
 	)
