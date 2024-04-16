4dbb51b6376e19e4c413be2c02b0ecf7fcb088f4
Nirzon Karmakar <nirzon47@gmail.com>
2024-04-16 12:10:45 +0530

    style: added stagger delay for skill section

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -143,7 +143,7 @@ const Skills = () => {
 			'.carousels',
 			isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: -100 },
 			{
-				delay: isInView ? stagger(0.1, { startDelay: 0.15 }) : 0,
+				delay: isInView ? stagger(0.3, { startDelay: 0.15 }) : 0,
 			}
 		)
 	}, [scope, animate, isInView])
